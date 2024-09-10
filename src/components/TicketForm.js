46ff1e641b0e4922853ea8f3345038613129dddd
saieesha <saieeshabonthala@gmail.com>
2024-09-10 14:02:37 -0500

    All features implemented

--- src/components/TicketForm.js
+++ src/components/TicketForm.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-export default function TicketForm({dispatch}) {
+export default function TicketForm({dispatch, editingTicket}) {
+
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState("");
+
+  useEffect(() => {
+    if(editingTicket){
+      setTitle(editingTicket.title)
+      setDescription(editingTicket.description)
+      setPriority(editingTicket.priority)
+    }else{
+      clearForm();
+    }
+  }, [editingTicket]);
 
   const priorityLabels = {
     1: "low",
@@ -21,19 +32,24 @@ export default function TicketForm({dispatch}) {
     event.preventDefault();
 
     const ticketData = {
-      id: new Date().toISOString(), 
+      id: editingTicket? editingTicket.id : new Date().toISOString(), 
       title, 
       description,
       priority,
     };
   
     dispatch({
-      type:"ADD_TICKET",
+      type: editingTicket? "UPDATE_TICKET" : "ADD_TICKET",
       payload: ticketData
     });
 
     clearForm();
   };
+
+  const handleCancel = () => {
+    dispatch({type: "DELETE_EDITING_TICKET"})
+    clearForm();
+  }
 
   const handleDescriptionChange = (event) => {
     setDescription(event.target.value);
@@ -87,7 +103,19 @@ export default function TicketForm({dispatch}) {
           ))}
         </fieldset>
 
-        <button type="submit" className="button">submit</button>
+        <button type="submit" className="button">
+          submit
+        </button>
+
+        {editingTicket ? (
+          <button type="button" onClick={handleCancel} className="button">
+            cancel edit
+          </button>
+        ) : (
+          <button type="button" onClick={clearForm} className="button">
+            cancel submit
+          </button>
+        )}
 
       </form>
     </div>
